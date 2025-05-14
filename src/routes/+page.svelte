542cf80b380421b0c16ef7a57ad448d69037a54a
Tim Smith <tim.smith.hdg@gmail.com>
2025-05-14 11:17:02 -0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { ChyronWrapper } from "chyron-svelte";
+</script>
+
+<div class="image-container">
+  <img src="/imgs/IMG_7689.jpg" alt="Stop Sign">
+  <div class="overlay"></div>
+</div>
+
+<ChyronWrapper headline breaking logoLeft />
